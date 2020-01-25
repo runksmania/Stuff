e3f943eaad757c0e6c19c9513eaeaa59d3561e48
Michael Cottrell <runksmania1011@gmaill.com>
2020-01-25 02:36:31 -0800

    Combined words listed multiple times with different English Definitions together, and logic needed to make this work.
        get_pop_words.py
    Added logic to check if a word is already been seen, and add a translation if it has.
    Added logic to keep words in order using heap when creating output data.
    
        csv files
    Updated with changes generated from script due to combining repeated words.
    
        italianTopWordsGame.py & russianTopWordsGame.py
    Updated logic to work with new csv files.
    Added logic to show all possible answers if correct, and all answers if wrong.
    Added italics to word to guess.

--- Dictionary/get_pop_words.py
+++ Dictionary/get_pop_words.py
@@ -1,4 +1,5 @@
 import requests
+from heapq import heappop, heappush
 from bs4 import BeautifulSoup
 
 r = requests.get('https://1000mostcommonwords.com/1000-most-common-italian-words/')
@@ -8,13 +9,37 @@ soup = BeautifulSoup(r.content, 'html5lib')
 list_tds = [i.find_all('td') for i in soup.find_all('tr')]
 output = []
 
+word_dict = {}
+
 for i in list_tds:
-    output.append([i[1].get_text(), i[2].get_text()])
+
+    num = i[0].get_text()
+    non_english_word = i[1].get_text()
+    english_word = i[2].get_text()
+
+    if num != 'Number':
+        
+        if non_english_word not in word_dict:
+            word_dict[non_english_word] = [num, [english_word]]
+        else:
+            word_dict[non_english_word][1].append(english_word)
+
+for k,v in word_dict.items():
+
+    english_words = v[1][0]
+
+    if len(v[1]) > 1:
+        english_words = "\"" + ','.join(v[1]) + "\""
+
+    heappush(output, (int(v[0]), k, english_words))
 
 file_name = 'top1000ItalianWords.csv'
-#file_name = 'top1000RussianWords.csv'
+#file_name = './top1000RussianWords.csv'
 
 with open(file_name, mode='w', encoding='utf-8') as output_file:
-    
-    for i in output:
-        print(','.join(i), file=output_file)
+    print(','.join(['Number', 'Word', 'English Word(s)']), file=output_file)
+
+    for i in range(len(output)):
+        top = heappop(output)
+        num = str(top[0]) + ','
+        print(num + ','.join(top[1:]), file=output_file)
--- FlashCard Games/italianTopWordsGame.py
+++ FlashCard Games/italianTopWordsGame.py
@@ -7,13 +7,14 @@ data = process_csv('../Dictionary/top1000ItalianWords.csv')
 
 for i in data:
     
-    if i[1] != 'in English':
-        words_dict[i[0]] = i[1]
+    if i[0] != 'Number':
+      word_list = i[2].split(',')
+      words_dict[i[1]] = word_list
 
 user_input = ''
 non_english_list = []
 word_low_range = 0
-word_high_range = 1000
+word_high_range = len(data)
 
 for k,v in words_dict.items():
 
@@ -27,23 +28,23 @@ print('Enter 3 to play all 1000 words')
 user_input = input('Pick an option: ')
 
 if user_input == '2':
-    rand_amount = random.randint(101, 1000)
-    rand_index = random.randint(0, 1000)
+    rand_amount = random.randint(101, len(data))
+    rand_index = random.randint(0, len(data))
     coin_flip = random.randint(0,1)
 
     if coin_flip == 0:
         word_low_range = 0 if rand_index - rand_amount < 0 else rand_index - rand_amount
         word_high_range = word_low_range + 100 if word_low_range - rand_index < 100 else rand_index
-        word_high_range = 999 if word_high_range > 999 else word_high_range
+        word_high_range = len(data) - 1 if word_high_range > len(data) - 1 else word_high_range
 
     else:
-        word_high_range = 999 if rand_index + rand_amount > 999 else rand_index + rand_amount
+        word_high_range = len(data) - 1 if rand_index + rand_amount > len(data) - 1 else rand_index + rand_amount
         word_low_range = word_low_range - 100 if word_low_range + rand_index < 100 else rand_index
         word_low_range = 0 if word_high_range < 0 else word_low_range
 
 elif user_input == '3':
     word_low_range = 0
-    word_high_range = 999
+    word_high_range = len(data) - 1
 
 else:
     word_low_range = 0
@@ -54,13 +55,24 @@ while user_input != 'quit':
   index = random.randint(word_low_range, word_high_range)
   word = non_english_list[index]
 
-  user_input = input('What is this word in English ' + word + ':\n')
+  user_input = input('What is this word in English ' + '\x1B[3m' + word + '\x1B[23m' + ':\n')
 
   if user_input != 'quit':
 
-    if user_input == words_dict[word]:
-      print('Correct.\n')
+    if user_input in words_dict[word]:
+      print('Correct.')
+
+      if len(words_dict[word]) > 1:
+        print('All correct answers: ' + ', '.join(words_dict[word]))
+
+      print()
 
     else:
-      print('Incorrect.')
-      print('Correct answer: ' + words_dict[word] + '\n')
+      print('Incorrect.')\
+
+      if len(words_dict[word]) > 1:
+        print('Correct answers: ' + ', '.join(words_dict[word]))
+      else:
+        print('Correct answer: ' + ''.join(words_dict[word]))
+      
+      print()
